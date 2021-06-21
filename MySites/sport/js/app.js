5e40b4d122c7ee0acb182b826f5f82a510ae0a64
Evgenii Home <jon-95@list.ru>
2021-06-21 14:12:32 +0300

    валидация сайта

--- MySites/sport/js/app.js
+++ MySites/sport/js/app.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
             $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                $('.catalog-item__back-face').eq(i).toggleClass('catalog-item__back-face_active');
             })
         });
 
