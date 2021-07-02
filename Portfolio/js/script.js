ace4c11f2832fa2264014370a73af79cafda00c2
Evgenii <jon-95@list.ru>
2021-07-02 21:14:48 +0300

    Добавлен блок с контактами

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,3 +1,6 @@
+
+// Активное меню
+
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     overlay = document.querySelector('.overlay'),
@@ -10,4 +13,13 @@ hamburger.addEventListener('click', () =>{
 closeEl.addEventListener('click', () =>{
     menu.classList.remove('menu__active', 'animate__animated', 'animate__bounceInLeft');
     overlay.classList.remove('overlay__active');
+});
+
+// статус навыков
+
+const progress = document.querySelectorAll('.progress__status'),
+    lines = document.querySelectorAll('.progress__line-status');
+
+progress.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
